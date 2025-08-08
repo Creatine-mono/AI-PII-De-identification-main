e7492b22d13cf368268ccdb7fb82969432e84419
Creatine-mono <psh243360@gmail.com>
2025-08-08 14:09:19 +0900

    Update ai-gen-llama3.py

--- gen-data/ai-gen-llama3.py
+++ gen-data/ai-gen-llama3.py
@@ -48,26 +48,12 @@ def clear_memory():
 
 
 def load_model(model_path: str, *, quantize: bool = False):
-    if quantize:
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_path,
-            device_map="auto",
-            load_in_8bit=True,
-            torch_dtype=torch.float16,
-        )
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
-        pipe = transformers.pipeline(
-            "text-generation",
-            model=model,
-            tokenizer=tokenizer,
-        )
-    else:
-        pipe = transformers.pipeline(
-            "text-generation",
-            model=model_path,
-            model_kwargs={"torch_dtype": torch.bfloat16},
-        )
-    return pipe
+    model_pipeline = transformers.pipeline(
+        "text-generation",
+        model=model_path,
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device="cuda",)
+    return model_pipeline
 
 
 def generate_texts(pipeline, generated_df, path_save):
