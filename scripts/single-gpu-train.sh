a7e950866b43501355f8e38674d48560fed2c333
Creatine-mono <psh243360@gmail.com>
2025-08-12 15:14:33 +0900

    Update single-gpu-train.sh

--- scripts/single-gpu-train.sh
+++ scripts/single-gpu-train.sh
@@ -6,6 +6,11 @@ CUDA_VISIBLE_DEVICES=0 python3 ./training/train_single_large.py --dir ./cfgs/sin
 # Run Python module 2 in the background
 CUDA_VISIBLE_DEVICES=1 python3 ./training/train_single_large.py --dir ./cfgs/single-gpu --name cfg1.yaml &
 
+
+CUDA_VISIBLE_DEVICES=0 python3 ./training/train_single_large.py \
+    --jsonl_path ./data/mdd-gen/llama3_placeholder_2.3K_v0.jsonl 
+
+    
 # Wait for both processes to finish
 wait
 
